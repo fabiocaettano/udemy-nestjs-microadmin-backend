bf757e2bc865ad992c933b89d87630109c140038
fabiocaettano <fabio.caettano74@gmail.com>
2022-01-21 18:57:17 -0300

    configuracao do arquivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common'
+
+const logger = new Logger('Main')
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule,{
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://user:bitnami@127.0.0.1:5672/smartranking'],
+      queue: 'admin-backend'
+    },
+  });
+
+  app.useLogger(logger)
+  
+  await app.listen()
 }
 bootstrap();
